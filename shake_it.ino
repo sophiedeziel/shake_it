9730c110f35ed3abf7ad79f96461791460630405
Sophie Deziel <courrier@sophiedeziel.com>
2018-08-26 20:22:49 -0400

    Detecter un shake

--- shake_it.ino
+++ shake_it.ino
@@ -8,13 +8,80 @@
 
 Adafruit_SSD1306 display(4);
 
+const int MPU_addr = 0x68;
+int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
+
+// game modes:
+// 0: start
+// 1: jeu
+// 2: dead
+int gameMode = 0;
+
 void setup() {
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   Serial.begin(115200);
 }
 
 void loop() {
   display.clearDisplay();
-  display.drawRect(0, 16, 128, 48, WHITE);
+  switch (gameMode) {
+    case 0:
+      display.setTextSize(2);
+      display.setTextColor(WHITE);
+      display.setCursor(0, 25);
+      display.println("ShakeIT to begin");
+
+      detectShake();
+      break;
+    case 1:
+      display.setTextSize(2);
+      display.setTextColor(WHITE);
+      display.setCursor(0, 25);
+      display.println("GAME");
+      break;
+    case 2:
+
+      break;
+  }
+
   display.display();
+
+
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+
+  Wire.requestFrom(MPU_addr, 14, true); // request a total of 14 registers
+  AcX = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+  AcY = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ = Wire.read() << 8 | Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp = Wire.read() << 8 | Wire.read(); // 0x41 (TEMP_OUT_H)   & 0x42 (TEMP_OUT_L)
+  GyX = Wire.read() << 8 | Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY = Wire.read() << 8 | Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ = Wire.read() << 8 | Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+
+  Serial.print(AcX);
+  Serial.print(" ");
+
+  Serial.print(AcY);
+  Serial.print(" ");
+
+  Serial.println(AcZ);
+
+
+}
+
+void detectShake() {
+  static int last_z;
+
+  if (last_z - AcZ > 1000) {
+    gameMode = 1;
+  }
 }
